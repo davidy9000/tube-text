09015152ddfd74965f3fcb5e492fac2f1427fcfd
davidy9000 <davidy.9000@gmail.com>
2020-01-17 15:42:24 -0500

    created allNotes reducer

---
+++ client/src/reducers/allNotes.js
@@ -0,0 +1,11 @@
+import { fetchNotesThunk } from '../actions/actionCreators';
+import * as type from '../actions/actionTypes';
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case type.FETCH_NOTES:
+            return action.payload;
+        default:
+            return state;
+    }
+}
