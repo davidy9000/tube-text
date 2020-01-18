d3d41b7fec81dd968bc8cc78a273b55c479a5b95
Brittany Cho <bac411@nyu.edu>
2020-01-18 17:29:25 -0500

    edited single session view to add form

--- client/src/components/views/SingleSessionView.jsx
+++ client/src/components/views/SingleSessionView.jsx
@@ -4,12 +4,26 @@ import { Link } from 'react-router-dom';
 const SingleSessionView = (props) => {
     const {allNotes, addNotesThunk} = props;
 
+    let onSubmitNote 
     // addNotesThunk()
     return (
         <div className="App">
             {allNotes.map((notes)=>{
             return <p>{notes.noteRecord}</p>
             })}
+
+            <form>
+                {/* Temporary Study Session field */}
+                Study Session: <input type="text" name="study-session"></input>
+                <br/>
+                {/* Temporary TimeStamp field */}
+                TimeStamp: <input type="text" name="timestamp"></input>
+                <br/>
+                Note: <input type="text" name="note"></input>
+                <br/>
+                <input type="submit" onSubmit={addNotesThunk()}></input>
+
+            </form>
             
         </div>
 
