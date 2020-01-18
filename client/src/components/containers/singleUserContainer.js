6836cbfe966e065c1edf5e4bcab19e821a4d3314
Suchwinder Singh <windersingh80@yahoo.com>
2020-01-18 13:52:31 -0500

    Modificatoins to layout,including comments to imports

--- client/src/components/containers/singleUserContainer.js
+++ client/src/components/containers/singleUserContainer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import SingleUserView from './singleUserView';
 import { connect } from 'react-redux';
-import { fetchSessionsThunk } from '../../actions/actionCreatorsThunks';
+import { fetchSessionsThunk } from '../../store/actions/actionCreatorsThunks';
 
-class SingleUserContainer extends Component {
-    render(){
-        return({
-            <SingleUserView/>
-        })
-    }
-}
+// class SingleUserContainer extends Component {
+//     render(){
+//         return{
+//             <div>
+//             Hello World </div>
+//         }
+//     }
+// }
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,25 @@
+/**
+ * These imports are needed in order to have our "single-source-of-truth" set up,
+ * middleware set up (including thunks for asynchronous calls), loggings for the 
+ * actions being called etc...
+ */
 import { combineReducers, applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+/**
+ * Need our reducers to combine into a single root reducer
+ */
 import allNotes from "./reducers/allNotes";
 import userSessions from './reducers/userSessions';
 
+/**
+ * Creating rootReducer that will have all the functionality of our SPA
+ * Creating a logger to display the actions taken adn changes to state (pre, ation, post)
+ * Creating a middleware to handle async calls
+ * Creating the store that houses our state (single-source-of-truth)
+ */
 const rootReducer = combineReducers({allNotes, userSessions});
 const logger = createLogger({ collapsed: true });
 const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, logger));
