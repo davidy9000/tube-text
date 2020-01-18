614fb34f0298c81223e04386c1486c450b656f7a
Suchwinder Singh <windersingh80@yahoo.com>
2020-01-18 15:29:18 -0500

    Single Session start

---
+++ client/src/components/containers/singleSessionContainer.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import SingleUserView from '../views/singleUserView';
+import { connect } from 'react-redux';
+import { fetchSessionsThunk } from '../../store/actions/actionCreatorsThunks';
