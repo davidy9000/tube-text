91935aa9d06d176ee0f0f76f5531c9d5d3b1a0b7
Brittany Cho <bac411@nyu.edu>
2020-01-20 15:34:43 -0500

    added Fragment for Responsive functionality

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -135,17 +135,21 @@ const App = (props) => {
 
         return (
             <div>
-				<AppBar position="static" className = {classes.customizeAppBar}>
-                <Toolbar className = {classes.customizeToolBar}>
-                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton> */}
-                    <Typography variant="h6" className={classes.title} color = "inherit">
-                        TubeText
-                    </Typography>
-                    <Button className = {classes.customNavButton}>Sign Out</Button>
-                </Toolbar>
-            	</AppBar>
+                <div style={{borderBottom: '5px solid white'}}>
+                    <AppBar position="static" className = {classes.customizeAppBar}>
+                    <Toolbar className = {classes.customizeToolBar}>
+                        {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton> */}
+                        <Typography variant="h6" className={classes.title} color = "inherit">
+                            TubeText
+                        </Typography>
+                        <Button className = {classes.customNavButton}>Sign Out</Button>
+                    </Toolbar>
+                    </AppBar>
+                </div>
+                {/* <div className="separate-nav" style={{backgroundColor: "white"}}></div> */}
+
 				<Router>
 					<Switch>
 						{/* <div className="App">
--- client/src/components/views/SingleSessionView.jsx
+++ client/src/components/views/SingleSessionView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Fragment} from 'react';
 // import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
@@ -11,6 +12,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CreateIcon from '@material-ui/icons/Create';
+import Responsive from 'react-responsive';
 
 // import { palette, spacing, typography } from '@material-ui/system';
 // import styled from 'styled-components';
@@ -23,6 +25,11 @@ import '../../singleSessionCSS.css';
 import Youtube from 'react-youtube';
 
 
+//Responsive functionality
+const Desktop = props => <Responsive {...props} minWidth={1275} />;
+const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={1274} />;
+const Mobile = props => <Responsive {...props} maxWidth={767} />;
+
 //TEMPORARY FOR EDIT TESTING
 const fakeObject = {
     id: 1,
@@ -32,139 +39,134 @@ const fakeObject = {
 
 const SingleSessionView = (props) => {
     const { allNotes, handleChange, handleSubmit, deleteNote, editNote, videoUrl,
-    opts, videoOnReady, videoOnPlay, videoStateChange } = props;
+    opts, videoOnReady, videoOnPlay, videoStateChange, editNoteState, onClickEdit } = props;
     // console.log("The Hangle Change is: ", handleChange);
     return (
         <div className="overall-container">
-            {/* <div className="App"> */}
-            
-            {/* <Container className="Container">
-            <Box className="Box" m="auto"> */}
+            <Fragment>
+                <Desktop>
                 
-                {/* Divides first two grids to be rows */}
-                <Grid className="main-grid"
-                container 
-                container spacing={1} 
-                container direction="row"
-                justify="center"
-                alignItems="center" 
-                 > 
+                    {/* Divides first two grids to be rows */}
+                    <Grid className="main-grid"
+                    container 
+                    container spacing={1} 
+                    container direction="row"
+                    justify="center"
+                    alignItems="center" 
+                    > 
 
-                        {/* 1/2 Large Grid */}
-                        <Grid className="Grid" 
-                        container direction="column"
-                        item xs={6} 
-                       >
+                            {/* 1/2 Large Grid */}
+                            <Grid className="Grid" 
+                            container direction="column"
+                            item xs={6} 
+                        >
 
-                            <Grid item xs={6}>
+                                <Grid item xs={6}>
+                                    
+                                        <Youtube
+                                            videoId = {videoUrl}
+                                            opts={opts}
+                                            // onReady={videoOnReady}
+                                            onPlay={videoOnPlay}
+                                            onStateChange={videoStateChange}
+                                        />
+                                    
+                                </Grid>
+
+
+
+                                <Grid item xs={12} className="form-grid"
                                 
-                                    <Youtube
-                                        videoId = {videoUrl}
-                                        opts={opts}
-                                        // onReady={videoOnReady}
-                                        onPlay={videoOnPlay}
-                                        onStateChange={videoStateChange}
-                                    />
                                 
+                                >
+                                    <form onSubmit={handleSubmit}>
+                                        {/* Temporary TimeStamp field */}
+                                        <br/>
+                                        <label>TimeStamp: </label><br/>
+                                        <input type="text" name = "videoTimestamp" onChange={handleChange} ></input>
+                                        <br/><br/>
+                                        <label>Note: </label><br/>
+                                        <input type="text" name = "noteRecord" onChange={handleChange} style={{ minWidth: 400, minHeight: 50, overflow: 'auto'}} ></input>
+                                        <br/>
+                                        {/* <TextareaAutosize aria-label="minimum height" rowsMin={3} placeholder="Enter Note Here" name = "noteRecord"onChange={handleChange} 
+                                        style={{ minWidth: 400, maxHeight: 200, overflow: 'auto'}}/> */}
+                                        <br/>
+                                        <input className="note-record" type="submit"></input>
+
+                                    </form>
+                                </Grid>
                             </Grid>
 
+                            {/* 2/2 Large Grid */}
+                            <Grid className="Grid" 
+                            container spacing={3}
+                            item xs={6} 
+                            container direction="column"
+                            justify="center" 
+                            alignItems="center">
 
+                                {/* <div className="notes-wrapper"> */}
+                                <Paper style={{minHeight: 550, minWidth: 600, maxHeight: 550, overflow: 'auto', backgroundColor: 'white'}}>
+                                    <List className="List">
 
-                            <Grid item xs={12} className="form-grid"
-                            
-                            
-                            >
-                                <form onSubmit={handleSubmit}>
-                                    {/* Temporary TimeStamp field */}
-                                    <br/>
-                                    <label>TimeStamp: </label><br/>
-                                    <input type="text" name = "videoTimestamp" onChange={handleChange} ></input>
-                                    <br/><br/>
-                                    <label>Note: </label><br/>
-                                    <input type="text" name = "noteRecord" onChange={handleChange} style={{ minWidth: 400, minHeight: 50, overflow: 'auto'}} ></input>
-                                    <br/>
-                                    {/* <TextareaAutosize aria-label="minimum height" rowsMin={3} placeholder="Enter Note Here" name = "noteRecord"onChange={handleChange} 
-                                    style={{ minWidth: 400, maxHeight: 200, overflow: 'auto'}}/> */}
-                                    <br/>
-                                    <input className="note-record" type="submit"></input>
+    
+                                        {allNotes.map((note)=>{
+                                            return (
+                                                <ListItem className="ListItem"
+                                                alignItems="center">
+                                                    <Grid item xs={3} className="notes-grid">
+                                                        <div className="individual-note">
+                                                            <div className="note-buttons">
+                                                                
+                                                                <div className="edit-note">
+                                                                    <Button onClick = {() => editNote(fakeObject)} >
+                                                                    {/* <Button onClick={onClickEdit}> */}
 
-                                </form>
-                            </Grid>
-                        </Grid>
+                                                                            <IconButton aria-label="create" disabled color="secondary">
+                                                                                <CreateIcon />
+                                                                            </IconButton>
+                                                                    </Button>
+                                                                    {/* <button onClick = {() => editNote(fakeObject)}>{note.noteRecord}</button> */}
+                                                                </div>
 
-                        {/* 2/2 Large Grid */}
-                        <Grid className="Grid" 
-                        container spacing={3}
-                        item xs={6} 
-                        container direction="column"
-                        justify="center" 
-                        alignItems="center">
-
-                            {/* <div className="notes-wrapper"> */}
-                            <Paper style={{minHeight: 550, minWidth: 600, maxHeight: 550, overflow: 'auto', backgroundColor: 'white'}}>
-                                <List className="List">
-
-   
-                                    {allNotes.map((note)=>{
-                                        return (
-                                            <ListItem className="ListItem"
-                                            alignItems="center">
-                                                <Grid item xs={3} className="notes-grid">
-                                                    <div className="individual-note">
-                                                        <div className="note-buttons">
-                                                            
-                                                            <div className="edit-note">
-                                                                <Button onClick = {() => editNote(fakeObject)} >
-                                                                        <IconButton aria-label="create" disabled color="secondary">
-                                                                            <CreateIcon />
+                                                                <div className="delete-note">
+                                                                    <Button onClick = {() => deleteNote(note.id)} id = {note.id}>
+                                                                        <IconButton aria-label="delete" disabled color="primary">
+                                                                            <DeleteIcon />
                                                                         </IconButton>
-                                                                </Button>
-                                                                {/* <button onClick = {() => editNote(fakeObject)}>{note.noteRecord}</button> */}
+                                                                    </Button>
+                                                                    {/* <button onClick = {() => deleteNote(note.id)} id = {note.id}>Delete {note.noteRecord}</button> */}
+                                                                </div>
                                                             </div>
-
-                                                            <div className="delete-note">
-                                                                <Button onClick = {() => deleteNote(note.id)} id = {note.id}>
-                                                                    <IconButton aria-label="delete" disabled color="primary">
-                                                                        <DeleteIcon />
-                                                                    </IconButton>
-                                                                </Button>
-                                                                {/* <button onClick = {() => deleteNote(note.id)} id = {note.id}>Delete {note.noteRecord}</button> */}
+                                                            <div className="note-record">
+                                                                <p>{note.noteRecord}</p>
                                                             </div>
                                                         </div>
-                                                        <div className="note-record">
-                                                            <p>{note.noteRecord}</p>
-                                                        </div>
-                                                    </div>
-                                                </Grid>
-                                            </ListItem>
-                                            )
-                                        })}
-                                  
-                                </List>
-                            </Paper>
-                            {/* </div> */}
+                                                    </Grid>
+                                                </ListItem>
+                                                )
+                                            })}
+                                    
+                                    </List>
+                                </Paper>
 
-                            {/* <Grid item xs={3}>
-                                <div className="test-grid">Test</div>
                             </Grid>
-                            <Grid item xs={3}>
-                                <div className="test-grid">Test</div>
-                            </Grid>
-                            <Grid item xs={3}>
-                                <div className="test-grid">Test</div>
-                            </Grid> */}
+                    </Grid>
 
-                        </Grid>
-                        
-                        
-                    
-                </Grid>
+                </Desktop>
 
-                    
-                    
-                {/* </div> */}
-                {/* </Box>
-            </Container> */}
+
+                {/* RESPONSIVE TABLET VIEW */}
+                <Tablet>
+                    <div>Tablet ?????</div>
+                </Tablet>
+
+                {/* RESPONSIVE MOBILE VIEW */}
+                <Mobile>
+                <div>Mobile ?????</div>
+                </Mobile>
+
+            </Fragment>
         </div>
 
     )
